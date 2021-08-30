b051c5277e4d4d72e3ed8546ad550e1f947d8f5d
JaeHyun An <36184435+jaehyunan11@users.noreply.github.com>
2021-08-30 07:31:54 +0000

    print debugging

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-print("Hello World")
-nums = [1,2,3]
-for i in range(len(nums)):
-	print(nums[i])
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+# print("Hello World!\nHello WOrld!\nHello World!")
+print("Hello" + " " + "Jaehyun")
+print("Hello " + "Jaehyun")
+print("Hello" + " Jaehyun")
+
+# fix the code below
+print("Day1 - String manipulation")
+print("String Concatenation is done with the "'+'" sign.")
+print('e.g. print("Hello" + "world")')
+print("New lines can be createddwith a backslash and n.")
