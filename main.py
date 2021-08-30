b79943221ba30d38a77de3cf5604e78b1c358493
JaeHyun An <36184435+jaehyunan11@users.noreply.github.com>
2021-08-30 07:55:53 +0000

    variable with input

--- main.py
+++ main.py
@@ -2,12 +2,26 @@
 # print("The function is declared like this:")
 # print("print('what to print')")
 # print("Hello World!\nHello WOrld!\nHello World!")
-print("Hello" + " " + "Jaehyun")
-print("Hello " + "Jaehyun")
-print("Hello" + " Jaehyun")
+# print("Hello" + " " + "Jaehyun")
+# print("Hello " + "Jaehyun")
+# print("Hello" + " Jaehyun")
 
 # fix the code below
-print("Day1 - String manipulation")
-print("String Concatenation is done with the "'+'" sign.")
-print('e.g. print("Hello" + "world")')
-print("New lines can be createddwith a backslash and n.")
+# print("Day1 - String manipulation")
+# print("String Concatenation is done with the "'+'" sign.")
+# print('e.g. print("Hello" + "world")')
+# print("New lines can be createddwith a backslash and n.")
+
+# input function
+# input() will getduser input in console
+# Then print() will print the world "Hello" and the user input
+# print("Hello" + input("What is your name?"))
+
+# Write your code belw this line
+
+# print(len("Jaehyun"))
+# print(len(input ("What is your name?")))
+
+name = input("What is your name?")
+length = len(name)
+print(length)
